98c4ddfa04bde5d58d61ad1a3e71f8731a3f2511
Daniela Maria <daniela.galatan28@gmail.com>
2021-02-24 22:48:10 +0200

    Add author model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    """An entity that holds information about me, the author."""
+
+    name = models.CharField(max_length=100, default=None)
+    surname = models.CharField(max_length=100, default=None)
