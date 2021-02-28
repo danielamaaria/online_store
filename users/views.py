2f459386a360e8e7dfcfc5bd2cebd705fdd7b949
Daniela Maria <daniela.galatan28@gmail.com>
2021-02-28 19:27:55 +0200

    Fix register crash

--- users/views.py
+++ users/views.py
@@ -15,7 +15,7 @@ def register(request):
         new_user = form.save()
         # Log the user in and then redirect to home page.
         login(request, new_user)
-        return redirect('learning_logs:index')
+        return redirect('store:index')
 
     # Display a blank or invalid form.
     context = {'form': form}
