0f78da8649d4210693ad864f41fc5664e3e794ca
danielamaaria <daniela.galatan28@gmail.com>
2021-03-02 23:23:44 +0200

    Add Category parent and Book model and migrations

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from store.models import Author, Category
+from store.models import Author, Category, Book
 
 admin.site.register(Category)
 admin.site.register(Author)
+admin.site.register(Book)
--- store/models.py
+++ store/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Author(models.Model):
-    """An entity that holds information about me, the author."""
+    """An entity that describes an author."""
 
     name = models.CharField(max_length=100, default=None)
     surname = models.CharField(max_length=100, default=None)
@@ -14,10 +14,32 @@ class Author(models.Model):
 class Category(models.Model):
     """A model which describes the genre of a book. E.G. Drama, Comedy..."""
 
-    category_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    is_child = models.BooleanField(default=True)  # It means this category has books referencing it.
+    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
 
     class Meta:
         verbose_name_plural = 'categories'
 
     def __str__(self):
-        return self.category_name
+        if self.is_child:
+            return self.name + ' (child)'
+        return self.name + ' (parent)'
+
+
+class Book(models.Model):
+    """
+    A model which describes the book entity. Each Book has a
+    foreign key referencing the ID of a Category.
+    """
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    price = models.FloatField(default=0.0)
+
+    class Meta:
+        verbose_name_plural = 'books'
+
+    def __str__(self):
+        return self.title + ' By ' + str(self.author)
