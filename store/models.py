800fb33817d62df7f5ff20382a0ba2e36465c484
danielamaaria <daniela.galatan28@gmail.com>
2021-03-02 22:07:40 +0200

    Add Category model and migration file

--- store/models.py
+++ store/models.py
@@ -6,3 +6,18 @@ class Author(models.Model):
 
     name = models.CharField(max_length=100, default=None)
     surname = models.CharField(max_length=100, default=None)
+
+    def __str__(self):
+        return self.name + ' ' + self.surname
+
+
+class Category(models.Model):
+    """A model which describes the genre of a book. E.G. Drama, Comedy..."""
+
+    category_name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.category_name
